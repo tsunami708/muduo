3830429a7e2c4478e42bad0f63c3bc5aae8a1044
tsunami <chxsemail@163.com>
2025-09-05 15:42:43 +0800

    redo

--- base/include/nocopy.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-class nocopy_t {
-protected:
-    constexpr nocopy_t() = default;
-    ~nocopy_t() = default;
-    nocopy_t(const nocopy_t&) = delete;
-    nocopy_t& operator=(const nocopy_t&) = delete;
-};
